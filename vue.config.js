d90561163cfb22701d4b8bf207b92c5d881f5276
eljsh95 <eljsh95@naver.com>
2024-02-01 23:20:11 +0900

    scss 세팅

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      scss: {
+        //모든 변수 파일 연결
+        additionalData: `
+          @import "@/assets/scss/base/_index.scss";
+          @import "@/assets/scss/helpers/_index.scss";
+          @import "@/assets/scss/layout/_index.scss";
+        `,
+      },
+    },
+  },
+});
