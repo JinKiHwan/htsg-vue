81e2aca892bbc6e97440c1931e39fb64fb246fad
eljsh95 <eljsh95@naver.com>
2024-02-02 15:34:05 +0900

    main레이아웃 옮기기

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,198 @@
+<template>
+  <header class="header">
+    <div class="header__inner">
+      <h1 class="header__logo">
+        <a href="/">
+          <img src="@/assets/img/header/logo.png" alt="현타삼국" />
+        </a>
+      </h1>
+
+      <ul class="header__gnb" id="gnb">
+        <li><a href="#main" class="on">메인</a></li>
+        <li><a href="#character">게임소개</a></li>
+        <li><a href="#gallery">갤러리</a></li>
+      </ul>
+
+      <ul class="header__sns">
+        <li>
+          <a
+            href="https://game.naver.com/lounge/hyuntasamguk/home"
+            target="_blank"
+            ><img
+              src="@/assets/img/header/sns-cafe.png"
+              alt="현타삼국 네이버 라운지"
+          /></a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/people/%ED%98%84%ED%83%80%EC%82%BC%EA%B5%AD/61552849521159/"
+            target="_blank"
+            ><img
+              src="@/assets/img/header/sns-facebook.png"
+              alt="현타삼국 페이스북"
+          /></a>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'headerComp',
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  height: remSet(90px);
+  background: linear-gradient(
+    0deg,
+    rgba(9, 27, 51, 0.9) 0%,
+    rgba(52, 69, 94, 0.9) 70%
+  );
+  position: fixed;
+  z-index: $z-header;
+  left: 0;
+  top: 0;
+
+  &__inner {
+    width: 100%;
+    height: 100%;
+    padding: 0 remSet(35px);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+
+    @media (max-width: 280px) {
+      padding: 0 remSet(15px);
+    }
+  }
+
+  &__logo {
+    @include mobile {
+      width: remSet(50px);
+    }
+  }
+
+  &__gnb {
+    display: flex;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: remSet(700px);
+
+    @include labtop {
+      width: auto;
+    }
+
+    li {
+      width: 33.3333%;
+      padding: 0 remSet(70px);
+      color: #fff;
+      position: relative;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      @include mobile {
+        padding: 0 remSet(35px);
+      }
+      @media (max-width: 280px) {
+        padding: 0;
+        &:nth-child(2) {
+          padding: 0 remSet(45px);
+        }
+      }
+
+      a {
+        font-size: remSet(22px);
+        transition: all 0.3s;
+        background: linear-gradient(
+          0deg,
+          rgba(173, 190, 205, 1) 0%,
+          rgba(209, 225, 236, 1) 100%
+        );
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        line-height: 1.5;
+
+        &.on {
+          font-size: remSet(24px) !important;
+          background: linear-gradient(
+            338deg,
+            rgba(188, 149, 70, 1) 0%,
+            rgba(245, 214, 108, 1) 60%
+          ) !important ;
+          background-clip: text !important;
+          -webkit-background-clip: text !important;
+          -webkit-text-fill-color: transparent !important;
+        }
+
+        &:hover {
+          font-size: remSet(24px);
+          background: linear-gradient(
+            338deg,
+            rgba(188, 149, 70, 1) 0%,
+            rgba(245, 214, 108, 1) 60%
+          );
+          background-clip: text;
+          -webkit-background-clip: text;
+          -webkit-text-fill-color: transparent;
+          @include mobile {
+            font-size: remSet(22px);
+            background: linear-gradient(
+              0deg,
+              rgba(173, 190, 205, 1) 0%,
+              rgba(209, 225, 236, 1) 100%
+            );
+            background-clip: text;
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+          }
+        }
+      }
+
+      &::after {
+        content: '';
+        display: block;
+        width: remSet(8px);
+        height: remSet(8px);
+        border: 1px solid #fff;
+        position: absolute;
+        right: 0;
+        top: 50%;
+        transform: translateY(-50%) rotate(45deg);
+        opacity: 0.5;
+
+        @media (max-width: 280px) {
+          display: none;
+        }
+      }
+
+      &:last-child {
+        &::after {
+          display: none;
+        }
+      }
+    }
+  }
+
+  &__sns {
+    display: flex;
+
+    li {
+      margin-left: remSet(20px);
+
+      @include mobile {
+        width: remSet(45px);
+        margin-left: remSet(10px);
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/MainComp.vue
@@ -0,0 +1,177 @@
+<template>
+  <section class="main" id="main">
+    <div class="main__bg">
+      <video
+        src="@/assets/img/main/mainVideo.mp4"
+        autoplay
+        muted
+        playsinline
+        loop
+        class="pc-show"
+        v-if="isDesktop"></video>
+      <video
+        src="@/assets/img/main/mainVideo-m.mp4"
+        autoplay
+        muted
+        playsinline
+        loop
+        class="mo-show"
+        v-else></video>
+    </div>
+    <!-- 메인 로고 및 사전예약 버튼 -->
+    <div class="main__inner">
+      <div class="main__logo" id="mainLogo">
+        <img src="@/assets/img/main/main-logo.png" alt="현타삼국" />
+      </div>
+      <ul class="main__store" id="mainBtn">
+        <li>
+          <a href="https://app.adjust.com/1784cnfn" target="_blank"
+            ><img src="@/assets/img/main/store-google.png" alt="구글스토어"
+          /></a>
+        </li>
+        <li>
+          <a href="https://app.adjust.com/17t6jr6k" target="_blank"
+            ><img src="@/assets/img/main/store-one.png" alt="원스토어"
+          /></a>
+        </li>
+        <li>
+          <a href="https://app.adjust.com/177qp7qm" target="_blank"
+            ><img src="@/assets/img/main/store-galaxy.png" alt="갤럭시스토어"
+          /></a>
+        </li>
+      </ul>
+    </div>
+    <!-- 스크롤 이미지 -->
+    <div class="main__scroll">
+      <img src="@/assets/img/main/scroll.png" alt="스크롤" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'MainComp',
+
+  data() {
+    return {
+      isDesktop: window.innerWidth > 1024,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.main {
+  /* background: url('#{$path-image}/main/main-bg.png') no-repeat center 100%;
+  background-size: 120% 120%; */
+  position: relative;
+
+  &__bg {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    z-index: -1;
+    width: 100%;
+    aspect-ratio: 1920/960;
+
+    @include labtop {
+      aspect-ratio: auto;
+      width: 100%;
+      height: 100%;
+    }
+
+    video {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      display: block;
+    }
+  }
+
+  &__inner {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    flex-direction: column;
+    padding-bottom: remSet(110px);
+  }
+
+  &__logo {
+    margin-bottom: remSet(25px);
+    opacity: 0;
+    transform: scale(0.95);
+
+    max-width: remSet(588px);
+    width: 30%;
+
+    @include mobile {
+      width: 100%;
+    }
+  }
+
+  &__btn {
+    background: url('#{$path-image}/main/main-btn-bg.png') no-repeat
+      center/cover;
+    width: remSet(400px);
+    height: remSet(100px);
+
+    a {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+      color: rgba(#fff6c1, 1);
+      font-size: remSet(28px);
+      font-weight: 800;
+      text-shadow: remSet(3px) remSet(2px) 0 rgba($color: #773f1d, $alpha: 1);
+    }
+  }
+
+  &__store {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    padding: 0 remSet(10px);
+    li {
+      margin: 0 remSet(5px);
+      max-width: calc(33.333333% - remSet(10px));
+
+      img,
+      a {
+        width: 100%;
+      }
+    }
+  }
+
+  &__scroll {
+    position: absolute;
+    bottom: remSet(30px);
+    left: 50%;
+    transform: translateX(-50%);
+
+    @include mobile {
+      width: remSet(50px);
+    }
+
+    img {
+      animation: scroll 1.2s infinite ease-in-out;
+    }
+  }
+}
+
+@keyframes scroll {
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(remSet(10px));
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+</style>
